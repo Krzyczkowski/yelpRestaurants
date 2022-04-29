faa0ccc989a71f3215871f243bfcd2af4848b1ff
K4am <krzyczkowski.wiktor00@gmail.com>
2022-04-29 20:05:54 +0200

    naprawiony react, dodany bootstrap + font awesome

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Home from "./routes/Home";
 import UpdatePage from "./routes/UpdatePage";
 import RestaurantDetailPage from "./routes/RestaurantDetailPage";
@@ -8,11 +8,11 @@ import RestaurantDetailPage from "./routes/RestaurantDetailPage";
 const App = () =>{
     return <div>
         <Router>
-            <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
-            <Route exact path="/restaurants/:id" component={RestaurantDetailPage}/>
-            </Switch>
+            <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/restaurants/:id/update" element={<UpdatePage/>}/>
+            <Route path="/restaurants/:id" element={<RestaurantDetailPage/>}/>
+            </Routes>
         </Router>
 
     </div>;

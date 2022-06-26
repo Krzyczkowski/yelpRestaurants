b944a2b0afb60696ce8c14ad899d82670ddf484f
K4am <krzyczkowski.wiktor00@gmail.com>
2022-06-26 22:45:42 +0200

    front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Home from "./routes/Home";
 import UpdatePage from "./routes/UpdatePage";
--- client/src/context/RestaurantsContext.js
+++ client/src/context/RestaurantsContext.js
@@ -1,3 +1,16 @@
 import React, {useState,createContext} from "react";
 
 export const RestaurantsContext = createContext();
+
+export const RestaurantsContextProvider = props => {
+    return (
+        
+
+
+
+
+        <RestaurantsContext.Provider>
+            {props.children}
+        </RestaurantsContext.Provider>
+    )
+}
